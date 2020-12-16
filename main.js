7c76bc9642f20baf1fea0e67ddf04d8f6d7a4a5e
Calvin <calvin.barrett@hotmail.com>
2020-12-16 17:22:25 -0600

    playing around with JS

--- main.js
+++ main.js
@@ -22,19 +22,38 @@
     return rect.top >= 0;
   };
 
+  let currentPosition = [0, 0];
+  let lastPosition = [0, 0];
+
+  $('.navbar li').on('click', (e) => {
+    lastPosition = [currentPosition[0], currentPosition[1]];
+    let Y = (document.body.scrollTop || document.documentElement.scrollTop);
+    currentPosition = [e.pageX, Y];
+    console.log("click", currentPosition, lastPosition);
+  })
+
+
+
+
+
+
   document.addEventListener(
     "wheel",
     function(event) {
       var $currentSlide = $($slides[currentSlide]);
-
+      // lastPosition = [currentPosition[0], currentPosition[1]];
+      let Y = (document.body.scrollTop || document.documentElement.scrollTop);
+      currentPosition = [event.pageX, Y];
+      console.log("wheel", currentPosition, lastPosition);
       if (isAnimating) {
         event.preventDefault();
         return;
       }
 
-      var direction = -event.deltaY;
-
-      if (direction < 0) {
+      // var direction = -event.deltaY;
+      var direction = currentPosition[1] - lastPosition[1];
+      console.log(direction);
+      if (direction < 0 || (lastPosition[1] === 0 && currentPosition[1] === 0)) {
         // next
         if (currentSlide + 1 >= $slides.length) return;
         if (!bottomIsReached($currentSlide)) return;
@@ -50,7 +69,7 @@
           1000,
           stopAnimation
         );
-      } else {
+      } if (direction > 0) {
         // back
         if (currentSlide - 1 < 0) return;
         if (!topIsReached($currentSlide)) return;
--- scripts.js
+++ scripts.js
@@ -1,91 +1,63 @@
-// H1 effect on load
-// const text = document.querySelector('.title-name');
-// const stringText = text.textContent;
 
-// const splitText = stringText.split('');
-// // console.log(splitText);
-// text.textContent = "";
+// H1 effect on hover
+function flowText() {
+  const text = document.querySelector('.title-name');
+  const stringText = text.textContent;
 
-// for (let i=0; i < splitText.length; i++) {
-//   text.innerHTML += `<span>${splitText[i]}</span>`;
-// }
+  const splitText = stringText.split('');
+  text.textContent = "";
+  
+  // break the h1 into individual spans
+  for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += `<span>${splitText[i]}</span>`;
+  }
 
-// let char = 0;
-// let timer = setInterval(onTick, 100);
+  // flip the color to orange
+  text.onmouseover = function () {
+    let char = 0;
+    let timer = setInterval(onTick, 100);
 
-// function onTick() {
-//   const span = text.querySelectorAll('span')[char];
-//   span.classList.add('fade');
-//   char++;
-//   if (char === splitText.length) {
-//     complete();
-//     return;
-//   }
-// }
+    function onTick() {
+      // add/remove class until the all the characters have changed
+      const span = text.querySelectorAll('span')[char];
+      span.classList.add('fade');
+      span.classList.remove('fadeBack');
+      char++;
+      if (char === splitText.length) {
+        complete();
+        return;
+      }
+    }
 
-// function complete() {
-//   clearInterval(timer);
-//   timer = null;
-// }
+    function complete() {
+      clearInterval(timer);
+      timer = null;
+    }
+  };
 
+  // flip the colour to blue/black
+  text.onmouseout = function () {
+    let char = 0;
+    let timer = setInterval(onTick, 100);
 
-// Mouse Over Effect on h1
-// $('.title-name')
-//   .mouseenter( function() {
-//     const text = document.querySelector('.title-name');
-//     const stringText = text.textContent;
+    function onTick() {
+      // add/remove class until the all the characters have changed
+      const span = text.querySelectorAll('span')[char];
+      span.classList.add('fadeBack');
+      span.classList.remove('fade');
+      char++;
+      if (char === splitText.length) {
+        complete();
+        return;
+      }
+    }
 
-//     const splitText = stringText.split('');
-//     // console.log(splitText);
-//     text.textContent = "";
-
-//     for (let i = 0; i < splitText.length; i++) {
-//       text.innerHTML += `<span>${splitText[i]}</span>`;
-//     }
-
-//     let char = 0;
-//     let timer = setInterval(onTick, 100);
-
-//     function onTick() {
-//       const span = text.querySelectorAll('span')[char];
-//       span.classList.add('fade');
-//       span.classList.remove('fadeBack');
-//       char++;
-//       if (char === splitText.length) {
-//         complete();
-//         return;
-//       }
-//     }
-
-//     function complete() {
-//       clearInterval(timer);
-//       timer = null;
-//     }
-//   })
-//   .mouseleave(function () {
-//     const text = document.querySelector('.title-name');
-
-//     let char = 0;
-//     let timer = setInterval(onTick, 100);
-
-//     function onTick() {
-//       const span = text.querySelectorAll('span')[char];
-//       console.log(span);
-//       span.classList.add('fadeBack');
-//       span.classList.remove('fade');
-//       char++;
-//       if (char === splitText.length) {
-//         complete();
-//         return;
-//       }
-//     }
-
-//     function complete() {
-//       clearInterval(timer);
-//       timer = null;
-//     }
-//   });
-
+    function complete() {
+      clearInterval(timer);
+      timer = null;
+    }
+  };
+}
 
 // Type Effect
 function typeEffect() {
@@ -100,6 +72,7 @@ function typeEffect() {
   
   function type() {
     if (charIndex < textArray[textArrayIndex].length) {
+      // start inserting the text one character at a time with a .2ms delay
       typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
       charIndex++;
       setTimeout(type, typingDelay);
@@ -148,17 +121,20 @@ function smoothScroll() {
 // Pop-up for each Project Effect
 function modalPopUp() {
   let infoToggle = document.getElementsByClassName('info-toggle');
-
   let close = document.getElementsByClassName('close');
 
+  // loop through the infoToggle div to get each node
   Array.from(infoToggle).forEach((item) => {
     item.addEventListener('click', function () {
+      // on click set the sibling class to active so only that specific modal is open
       this.nextElementSibling.classList.add('background-active');
     });
   });
 
+  // loop through each close class to get each node
   Array.from(close).forEach((item) => {
     item.addEventListener('click', function () {
+      // remove the background-active to whatever node is open to close the modal
       document.querySelector('.background-active').classList.remove('background-active');
     });
   });
@@ -168,34 +144,53 @@ function modalPopUp() {
 function activeLink() {
   const list = document.querySelectorAll('.navbar li');
 
+  // loop through to access each node
   list.forEach(items => {
     items.addEventListener('click', function () {
+      // remove the current light class and add it to destinate id span
       document.querySelector('.light').classList.remove('light');
       this.querySelector('span').classList.add('light');
     });
   });
-}
+};
 
+// Active page on scroll
+function activeScroll() {
+  // Create an event listener to the scroll to hightlight active
+  window.addEventListener('scroll', function () {
+    let home = document.getElementById('home').offsetTop;
+    let about = document.getElementById('about-me').offsetTop;
+    let skills = document.getElementById('skills').offsetTop;
+    let projects = document.getElementById('projects').offsetTop;
+    let contact = document.getElementById('contact').offsetTop;
+
+    // if statement based on id tags of each section to add and remove light class
+    if (window.scrollY >= home) {
+      document.querySelector('.light').classList.remove('light');
+      document.querySelector('span[data-page=home]').classList.add('light');
+    }
+    if (window.scrollY >= about) {
+      document.querySelector('.light').classList.remove('light');
+      document.querySelector('span[data-page=about]').classList.add('light');
+    }
+    if (window.scrollY >= skills) {
+      document.querySelector('.light').classList.remove('light');
+      document.querySelector('span[data-page=skills]').classList.add('light');
+    }
+    if (window.scrollY >= projects) {
+      document.querySelector('.light').classList.remove('light');
+      document.querySelector('span[data-page=projects]').classList.add('light');
+    }
+    if (window.scrollY >= contact) {
+      document.querySelector('.light').classList.remove('light');
+      document.querySelector('span[data-page=contact]').classList.add('light');
+    }
+  });
+};
+
+flowText();
 smoothScroll();
 typeEffect();
 modalPopUp();
 activeLink();
-
-window.addEventListener('scroll', function () {
-  let nav = document.querySelectorAll('.navbar span');
-  // let fromTop = window.scrollY;
-
-  nav.forEach(items => {
-    // let section = document.querySelector(items.hash);
-    console.log(items);
-    // if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
-
-    //   items.classList.add('light');
-
-    // } else {
-
-    //   items.classList.remove('light');
-
-    // }
-  })
-});
+activeScroll();
